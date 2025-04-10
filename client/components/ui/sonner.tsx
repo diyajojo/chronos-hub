ebc487b0d9303edf68093569aa13d2a1fd088798
diyajojo <diyaj2122@gmail.com>
2025-04-11 01:28:42 +0530

    added shadcn ui elements

---
+++ client/components/ui/sonner.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, ToasterProps } from "sonner";
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
+
+  return (
+    <Sonner
+      theme={theme as "light" | "dark" | "system"}
+      className="toaster group"
+      duration={5000}
+     // dismissible={true}
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          closeButton:
+            "group-[.toast]:bg-transparent group-[.toast]:border-none group-[.toast]:hover:bg-muted",
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
